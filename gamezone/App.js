7c0606371dec9800d18dae9e03cad3d9c7c7e307
jawad727 <jawaddhussein@gmail.com>
2020-04-14 22:26:43 -0700

    basic app set up with expo, all components imported to app.js

---
+++ gamezone/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from "./screens/Home"
+import ReviewDetails from "./screens/ReviewDetails"
+import About from "./screens/About"
+
+export default function App() {
+  return (
+    <View>
+      <Home />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
