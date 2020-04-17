31f52bbf42ced3697b221bc69dadf8db48785d07
jawad727 <jawaddhussein@gmail.com>
2020-04-16 22:09:02 -0700

    We can now navigate between screens with the react navigator

--- gamezone/screens/ReviewDetails.js
+++ gamezone/screens/ReviewDetails.js
@@ -1,11 +1,18 @@
 import React from "react"
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, Text, Button } from "react-native"
 import { globalStyles } from "../styles/globalStyles"
 
-export default function ReviewDetails() {
+export default function ReviewDetails({navigation}) { // We get navigation as a prop because of we configured it in the stack navigator
+
+    const pressHandler = () => {
+        navigation.goBack()
+    }
+
+
     return (
         <View style={globalStyles.container}>
             <Text>ReviewDetails Screen</Text>
+            <Button title="Back to home screen" onPress={pressHandler} />
         </View>
     )
 }
