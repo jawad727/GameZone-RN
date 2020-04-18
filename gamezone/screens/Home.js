cc7db68d730c38a516ad7d284a2bbc620e25007c
jawad727 <jawaddhussein@gmail.com>
2020-04-17 19:06:30 -0700

    users can now review details when clicking on a name (view it in detail)

--- gamezone/screens/Home.js
+++ gamezone/screens/Home.js
@@ -1,8 +1,14 @@
-import React from "react"
-import { StyleSheet, View, Text, Button } from "react-native"
+import React, { useState } from "react"
+import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity } from "react-native"
 import { globalStyles } from "../styles/globalStyles"
 
 export default function Home({ navigation }) {
+
+    const [reviews, setReviews] = useState([
+        {title: "Zelda", rating: 5, body: "lorem ipsum", id: 1},
+        {title: "Call Of Duty", rating: 4, body: "lorem ipsum", id: 2},
+        {title: "GTA 5", rating: 3, body: "lorem ipsum", id: 3},
+    ])
 
     const pressHandler = () => {
         navigation.push("ReviewDetails")
@@ -10,18 +16,16 @@ export default function Home({ navigation }) {
 
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText} >Home Screen</Text>
+            <FlatList  
+                data={reviews}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => ( 
+                    <TouchableOpacity onPress={() => navigation.navigate("ReviewDetails", item)} >
+                        <Text style={globalStyles.titleText} >{ item.title }</Text>
+                    </TouchableOpacity>
+                )}
+            />
             <Button title="go to review details" onPress={pressHandler} />
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    },
-    titleText: {
-        fontFamily: "nunito-bold",
-        fontSize: 18
-    }
-})
--- gamezone/screens/ReviewDetails.js
+++ gamezone/screens/ReviewDetails.js
@@ -11,14 +11,10 @@ export default function ReviewDetails({navigation}) { // We get navigation as a 
 
     return (
         <View style={globalStyles.container}>
-            <Text>ReviewDetails Screen</Text>
-            <Button title="Back to home screen" onPress={pressHandler} />
+            <Text>{ navigation.getParam("title") }</Text>
+            <Text>{ navigation.getParam("body") }</Text>
+            <Text>{ navigation.getParam("rating") }</Text>
+            {/* <Button title="Back to home screen" onPress={pressHandler} /> */}
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    }
-})
