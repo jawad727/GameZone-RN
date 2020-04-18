d7f39f9358718b2d59855cf5d1e10041964906f7
jawad727 <jawaddhussein@gmail.com>
2020-04-17 19:16:15 -0700

    applied global styles to all screen by using DefaultNavigationOptions

--- gamezone/routes/homeStack.js
+++ gamezone/routes/homeStack.js
@@ -5,13 +5,27 @@ import ReviewDetails from "../screens/ReviewDetails"
 
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: "GameZone",
+        }
     },
     ReviewDetails: {
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: "Review Details"
+        }
     }
 }
 
-const HomeStack = createStackNavigator(screens)
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: { // these are the default styles that show on everything unless individually overriden
+        headerTintColor: "#444",
+        headerStyle: {
+            backgroundColor: "#eee",
+            height: 60
+        }
+    }
+})
 
 export default createAppContainer(HomeStack)
