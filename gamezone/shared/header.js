4c24652ca65059825034aa6ad9ba2de7b4a5c29d
jawad727 <jawaddhussein@gmail.com>
2020-04-21 13:49:01 -0700

    GameZone logo added and header properly styled with bgImage

--- gamezone/shared/header.js
+++ gamezone/shared/header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StyleSheet, View, Text, Button } from "react-native"
+import { StyleSheet, View, Text, Button , Image, ImageBackground} from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 
 export default function Header({ navigation, title }) {
@@ -9,12 +9,13 @@ export default function Header({ navigation, title }) {
     }
 
     return (
-        <View style={styles.header}>
+        <ImageBackground style={styles.header} source={require("../assets/game_bg.png")}>
             <MaterialIcons name="menu" size={28} onPress={openMenu} style={styles.icon} />
-            <View>
+            <View style={styles.headerTitle} >
+                <Image style={styles.headerImage} source={require("../assets/heart_logo.png")} />
                 <Text style={styles.headerText} > { title } </Text>
             </View>
-        </View>
+        </ImageBackground>
     )
 }
 
@@ -35,5 +36,13 @@ const styles = StyleSheet.create({
     icon: {
         position: "absolute",
         left: 16
+    },
+    headerImage: {
+        width: 26,
+        height: 26,
+        marginHorizontal: 10
+    },
+    headerTitle: {
+        flexDirection: "row"
     }
 })
