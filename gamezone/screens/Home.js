d5a07b36b6776a8cdd9e25cfa08ebe4a90a390f1
jawad727 <jawaddhussein@gmail.com>
2020-04-21 13:15:46 -0700

    added card component to home and details screens and styled it to look like a card

--- gamezone/screens/Home.js
+++ gamezone/screens/Home.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react"
 import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity } from "react-native"
 import { globalStyles } from "../styles/globalStyles"
+import Card from "../shared/card"
 
 export default function Home({ navigation }) {
 
     const [reviews, setReviews] = useState([
-        {title: "Zelsda", rating: 5, body: "lorem ipsum", id: 1},
+        {title: "Zelda", rating: 5, body: "lorem ipsum", id: 1},
         {title: "Call Of Duty", rating: 4, body: "lorem ipsum", id: 2},
         {title: "GTA 5", rating: 3, body: "lorem ipsum", id: 3},
     ])
@@ -18,7 +19,9 @@ export default function Home({ navigation }) {
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => ( 
                     <TouchableOpacity onPress={() => navigation.navigate("ReviewDetails", item)} >
-                        <Text style={globalStyles.titleText} >{ item.title }</Text>
+                       <Card>
+                            <Text style={globalStyles.titleText} >{ item.title }</Text>
+                       </Card>
                     </TouchableOpacity>
                 )}
             />
--- gamezone/screens/ReviewDetails.js
+++ gamezone/screens/ReviewDetails.js
@@ -1,14 +1,17 @@
 import React from "react"
 import { StyleSheet, View, Text, Button } from "react-native"
 import { globalStyles } from "../styles/globalStyles"
+import Card from "../shared/card"
 
 export default function ReviewDetails({navigation, route}) { // We get navigation as a prop because of we configured it in the stack navigator
 
     return (
         <View style={globalStyles.container}>
-            <Text>{ route.params.title }</Text>
-            <Text>{ route.params.body }</Text>
-            <Text>{ route.params.rating }</Text> 
+            <Card>
+                <Text>{ route.params.title }</Text>
+                <Text>{ route.params.body }</Text>
+                <Text>{ route.params.rating }</Text>
+            </Card>
         </View>
     )
 }
