4e2fd5db9194ef1a87d0790608a4f69fc4bc72bb
jawad727 <jawaddhussein@gmail.com>
2020-04-21 16:27:10 -0700

    added a modal that slides upon icon click

--- gamezone/screens/Home.js
+++ gamezone/screens/Home.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react"
-import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity } from "react-native"
+import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity, Modal } from "react-native"
 import { globalStyles } from "../styles/globalStyles"
 import Card from "../shared/card"
+import { MaterialIcons } from "@expo/vector-icons"
 
 export default function Home({ navigation }) {
 
-    const [reviews, setReviews] = useState([
+    const [ modalOpen, setModalOpen ] = useState(false)
+
+    const [ reviews, setReviews ] = useState([
         {title: "Zelda", rating: 5, body: "lorem ipsum", id: 1},
         {title: "Call Of Duty", rating: 4, body: "lorem ipsum", id: 2},
         {title: "GTA 5", rating: 3, body: "lorem ipsum", id: 3},
@@ -14,6 +17,27 @@ export default function Home({ navigation }) {
 
     return (
         <View style={globalStyles.container}>
+
+            <Modal visible={modalOpen} animationType="slide">
+                <MaterialIcons
+                    name="close"
+                    size={24}
+                    style={{ ...styles.modalToggle, ...styles.modalClose, }}
+                    onPress={() => setModalOpen(false)}
+                />
+
+                <View style={styles.modalContent}>
+                    <Text> Hello from the modal </Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                name="add"
+                size={24}
+                style={styles.modalToggle}
+                onPress={() => setModalOpen(true)}
+            />
+
             <FlatList  
                 data={reviews}
                 keyExtractor={(item) => item.id}
@@ -28,3 +52,30 @@ export default function Home({ navigation }) {
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+
+    modalToggle: {
+        margin: 10,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 6,
+        elevation: 3,
+        borderRadius: 10,
+        borderColor: "lightgrey",
+        alignSelf: "center",
+        backgroundColor: "#fff",
+        shadowOffset: { width: 1, height: 1 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 2,
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 20
+    },
+    modalContent: {
+        flex: 1
+    }
+})
