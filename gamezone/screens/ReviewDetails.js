3d1b491dfba63c9c48994af56990158926305659
jawad727 <jawaddhussein@gmail.com>
2020-04-21 13:42:39 -0700

    dynamic images rendering on reviewDetails screen

--- gamezone/screens/ReviewDetails.js
+++ gamezone/screens/ReviewDetails.js
@@ -1,17 +1,33 @@
 import React from "react"
-import { StyleSheet, View, Text, Button } from "react-native"
-import { globalStyles } from "../styles/globalStyles"
+import { StyleSheet, View, Text, Button, Image } from "react-native"
+import { globalStyles, images } from "../styles/globalStyles"
 import Card from "../shared/card"
 
 export default function ReviewDetails({navigation, route}) { // We get navigation as a prop because of we configured it in the stack navigator
+
+    const rating = route.params.rating
 
     return (
         <View style={globalStyles.container}>
             <Card>
                 <Text>{ route.params.title }</Text>
                 <Text>{ route.params.body }</Text>
-                <Text>{ route.params.rating }</Text>
+                <View style={style.rating}>
+                    <Text> GameZone Rating: </Text>
+                    <Image source={images.rating[rating]} />
+                </View>
             </Card>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    rating: {
+        flexDirection: "row",
+        justifyContent: "center",
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: "#eee"
+    },
+})
